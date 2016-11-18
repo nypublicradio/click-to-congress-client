7ebb75ed6a8830ce79ee9494d50e43a1ab87c8d3
Brian Whitton <me@brianwhitton.com>
2016-11-18 00:30:19 -0500

    phone number formatting

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,12 +3,23 @@ import fetch from 'fetch';
 import config from '../config/environment';
 import set from 'ember-metal/set';
 
-const API = config.environment === 'development' ? 'http://localhost:4000' : null;
+function formatPhoneNumber(number) {
+  let formatted = number.replace(/-/g, '');
+  if (!formatted.startsWith('1')) {
+    return '1' + formatted;
+  } else {
+    return formatted;
+  }
+  
+}
 
 export default Ember.Controller.extend({
   selectedLegislator: Ember.computed('legislators.@each.selected', function() {
     let legislators = this.get('legislators');
     return legislators.findBy('selected');
+  }),
+  myFormattedNumber: Ember.computed('phone', function() {
+    return formatPhoneNumber(this.get('phone'));
   }),
   
   actions: {
@@ -18,12 +29,14 @@ export default Ember.Controller.extend({
       set(legislator, 'selected', true);
     },
     lookup(zip) {
-      fetch(`${API}/api/lookup?zip=${zip}`)
+      fetch(`${config.API}/api/lookup?zip=${zip}`)
         .then(r => r.json())
         .then(j => this.set('legislators', j));
     },
     call(phone) {
-      fetch(`${API}/api/call?my_number=${this.get('phone')}&dial_out=${phone}`);
+      let my_number = this.get('myFormattedNumber');
+      let dial_out = formatPhoneNumber(phone);
+      fetch(`${config.API}/api/call?my_number=${my_number}&dial_out=${dial_out}`);
     }
   }
 });
