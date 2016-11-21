15565dd5a191e9532c0011cc72855cb2721181aa
Brian Whitton <me@brianwhitton.com>
2016-11-21 01:21:08 -0500

    adds env config for mirage

--- config/environment.js
+++ config/environment.js
@@ -30,6 +30,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV['ember-cli-mirage'] = {
+      enabled: process.env.MIRAGE_ENABLED === true
+    }
   }
 
   if (environment === 'test') {
