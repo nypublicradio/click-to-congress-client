8ad1728f55eb23382126e92db179e75e257aac0e
Brian Whitton <me@brianwhitton.com>
2017-02-28 08:55:51 -0500

    use i18n for party and title computeds

--- app/models/representative.js
+++ app/models/representative.js
@@ -1,42 +1,13 @@
 import DS from 'ember-data';
-import computed from 'ember-computed';
+import service from 'ember-service/inject';
 
 export default DS.Model.extend({
+  i18n: service(),
+
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   phone: DS.attr('string'),
   party: DS.attr('string'),
   title: DS.attr('string'),
   district: DS.attr('string'),
-  
-  fullTitle: computed('title', function() {
-    let title = this.get('title');
-    
-    switch(title) {
-      case 'Sen':
-        return 'Senator';
-      case 'Rep':
-        return 'Representative';
-      case 'Del':
-        return 'Delegate';
-      case 'Com':
-        return 'Com';
-      default:
-        return 'no title';
-    }
-  }),
-  fullParty: computed('party', function() {
-    let party = this.get('party');
-    
-    switch(party) {
-      case 'D':
-        return 'Democrat';
-      case 'R':
-        return 'Republican';
-      case 'I':
-        return 'Independent';
-      default:
-        return 'No Party';
-    }
-  })
 });
