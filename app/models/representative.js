47ba1c58a872c80227214569eea4199cb21a1efc
Brian Whitton <me@brianwhitton.com>
2017-03-17 18:26:40 -0400

    update attrs to match new endpoint
    get the whole name now
    derive title from office
    derive district from divisionId

--- app/models/representative.js
+++ app/models/representative.js
@@ -1,13 +1,39 @@
 import DS from 'ember-data';
 import service from 'ember-service/inject';
+import computed from 'ember-computed';
+import ordinalize from '../utils/ordinalize';
 
 export default DS.Model.extend({
   i18n: service(),
 
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
+  name: DS.attr('string'),
   phone: DS.attr('string'),
   party: DS.attr('string'),
-  title: DS.attr('string'),
-  district: DS.attr('string'),
+  office: DS.attr('string'),
+  title: computed('office', function() {
+    let office = this.get('office');
+    if (office === 'United States Senate') {
+      return 'Senator';
+    } else if (office.match('House of Representatives')) {
+      return 'Representative';
+    } else if (office.match('State Senate')) {
+      return 'State Senator';
+    } else if (office.match('State Assembly')) {
+      return 'Assembly Member';
+    } else if (office.match('Borough President')) {
+      return 'Borough President';
+    } else {
+      return office;
+    }
+  }),
+  district: computed('divisionId', function() {
+    let districtCode = this.get('divisionId').split('/')[3];
+    if (districtCode) {
+      let district = districtCode.split(':')[1];
+      
+      if (!isNaN(Number(district))) { // a real number
+        return ordinalize(district);
+      }
+    }
+  })
 });
