91d30535e219a1ff1fa02588a710d591991d03b1
Brian Whitton <me@brianwhitton.com>
2017-03-17 19:02:39 -0400

    camelize the key before looking for it
    clean up unused attrs fo the serializer

--- app/serializers/representative.js
+++ app/serializers/representative.js
@@ -2,9 +2,5 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.JSONSerializer.extend({
-  keyForAttribute(attr) { return Ember.String.underscore(attr); },
-  primaryKey: 'bioguide_id',
-//  normalizeQueryResponse(store, primaryModelClass, { results }, id, requestType) {
-//    return this._super(store, primaryModelClass, results, id, requestType);
-//  }
+  keyForAttribute(attr) { return Ember.String.camelize(attr); },
 });
