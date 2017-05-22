94406d7c9a7be41886d4d8002f8ceacf3acaac68
Brian Whitton <me@brianwhitton.com>
2017-05-21 20:35:11 -0400

    don't need ember data since the server updated

--- app/adapters/representative.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-import config from '../config/environment';
-
-export default DS.RESTAdapter.extend({
-  host: config.API,
-  namespace:`${config.API_NAMESPACE}/v1`,
-  pathForType: () => 'lookup'
-});
