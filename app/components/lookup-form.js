fe7fc00bd644ee73ffe61282d7fa8ab086a7b764
Brian Whitton <me@brianwhitton.com>
2018-04-02 09:24:29 -0400

    pass server error into lookup form for rendering

--- app/components/lookup-form.js
+++ app/components/lookup-form.js
@@ -5,12 +5,25 @@ export default Ember.Component.extend({
   classNames: ['lookup-form'],
   error: {},
   
+  serverErrorMessage: Ember.computed('serverError', function() {
+    let serverError = this.get('serverError');
+    if (!serverError) {
+      return;
+    }
+    if (serverError.message === "Failed to parse address") {
+      return 'Did not recognize address. Try adding more details like city, state, or ZIP code.';
+    } else {
+      return serverError.message;
+    }
+  }),
+
   submit() {
     this.send('getReps');
   },
   
   actions: {
     getReps() {
+      this.set('serverError', null);
       if (!this.get('address')) {
         this.set('error.address', 'Address required');
         return;
